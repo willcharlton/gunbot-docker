6d476651c0bbd31732bfb563fa0af3dcf9d1520c
William Charlton <will@williamcharltonengineering.com>
2022-06-30 23:22:39 -0500

    fix env vars

--- .github/workflows/build-envs.sh
+++ .github/workflows/build-envs.sh
@@ -1,2 +1,2 @@
-GUNBOT_VERSION=$( curl -s https://api.github.com/repos/GuntharDeNiro/BTCT/releases/latest | jq -r .tag_name )
-export GUNBOT_VERSION
+BUILD_ARG_GUNBOT_VERSION=$( curl -s https://api.github.com/repos/GuntharDeNiro/BTCT/releases/latest | jq -r .tag_name )
+export BUILD_ARG_GUNBOT_VERSION
