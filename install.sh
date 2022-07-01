a6a98f913b97d69beef148ebb9217222d9601784
William Charlton <will@williamcharltonengineering.com>
2022-06-30 23:25:20 -0500

    fix env vars

--- install.sh
+++ install.sh
@@ -6,7 +6,11 @@ function get_download_version {
     python3 -c "import os ; print( os.environ['GUNBOT_VERSION'].replace('v','').replace('.','') )" 
 }
 
-URL="https://github.com/GuntharDeNiro/BTCT/releases/download/${GUNBOT_VERSION}/gunthy_linux.zip"
+function get_latest_version {
+    curl -s https://api.github.com/repos/GuntharDeNiro/BTCT/releases/latest | jq -r .tag_name
+}
+
+URL="https://github.com/GuntharDeNiro/BTCT/releases/download/$(get_latest_version)/gunthy_linux.zip"
 echo "Downloading: $URL"
 
 wget $URL
